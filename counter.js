2ae71adeda774c853ed13d0c972c95e9b0ab399f
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-04 12:33:42 +0100

    Moved counter to different files, created a temperature converter

---
+++ counter.js
@@ -0,0 +1,26 @@
+
+
+const header = document.getElementById("header");
+const decreaseBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+const increaseBtn = document.getElementById("increase");
+
+let counter = 0;
+header.textContent = counter;
+
+function increaseNumber() {
+    counter++;
+    header.textContent = counter;
+}
+function decreaseNumber() {
+    counter--;
+    header.textContent = counter;
+}
+function resetNumber() {
+    counter = 0;
+    header.textContent = counter;
+}
+
+increaseBtn.addEventListener("click", increaseNumber);
+decreaseBtn.addEventListener("click", decreaseNumber);
+resetBtn.addEventListener("click", resetNumber);
--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 
-const header = document.getElementById("header");
-const decreaseBtn = document.getElementById("decrease");
-const resetBtn = document.getElementById("reset");
-const increaseBtn = document.getElementById("increase");
+const userTemp = document.getElementById("userTemp");
+const toFehrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelcius");
+const submitBtn = document.getElementById("submitBtn");
+const result = document.getElementById("result");
 
-let counter = 0;
-header.textContent = counter;
-
-function increaseNumber() {
-    counter++;
-    header.textContent = counter;
-}
-function decreaseNumber() {
-    counter--;
-    header.textContent = counter;
-}
-function resetNumber() {
-    counter = 0;
-    header.textContent = counter;
+function celToFahrenheit() {
+    const userInput = Number(userTemp.value);
+    if (userInput === NaN) {
+        result.textContent = `Please input a number`;
+    } else {
+        return ((userInput * (9/5)) + 32).toFixed(2);
+    }
 }
 
-increaseBtn.addEventListener("click", increaseNumber);
-decreaseBtn.addEventListener("click", decreaseNumber);
-resetBtn.addEventListener("click", resetNumber);
+function fahToCelcius() {
+    const userInput = Number(userTemp.value);
+    if (userInput === NaN) {
+        result.textContent = `Please input a number`;
+    } else {
+        return ((userInput - 32) * (5/9)).toFixed(2);
+    }
+}
+
+submitBtn.addEventListener("click", () => {
+    if (userTemp.value == "") {
+        result.textContent = `Please input a number`;
+    } else {
+        if (toFehrenheit.checked) {
+            result.textContent = `${celToFahrenheit()} ℉`;
+        } else if (toCelcius.checked) {
+            result.textContent = `${fahToCelcius()} ℃`;
+        } else {
+            result.textContent = `Please check one of the above options`;
+        }
+    }
+})
+
+
