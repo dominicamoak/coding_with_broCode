10cee8943b2c8d61b671bafe0c3ddff717953a6d
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-06 18:32:13 +0100

    Created a multi-Dice roller

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
 
-const userTemp = document.getElementById("userTemp");
-const toFehrenheit = document.getElementById("toFahrenheit");
-const toCelcius = document.getElementById("toCelcius");
-const submitBtn = document.getElementById("submitBtn");
-const result = document.getElementById("result");
 
-function celToFahrenheit() {
-    const userInput = Number(userTemp.value);
-    if (Number.isNaN(userInput)) {
-        return null;
-    } else {
-        return ((userInput * (9/5)) + 32).toFixed(2);
+const rollDiceBtn = document.getElementById("rollDiceBtn");
+
+function rollDice() {
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for (let i=0; i<numOfDice; i++) {
+        const pick = Math.floor(Math.random() * 6) + 1;
+        values.push(pick);
+        images.push(`<img src="dice_images/${pick}.png" alt="Dice ${pick}">`);
     }
+
+    diceResult.textContent = `Rolled dice: ${values.join(", ")}`;
+    diceImages.innerHTML = images.join(" ");
 }
 
-function fahToCelcius() {
-    const userInput = Number(userTemp.value);
-    if (Number.isNaN(userInput)) {
-        return null;
-    } else {
-        return ((userInput - 32) * (5/9)).toFixed(2);
-    }
-}
-
-submitBtn.addEventListener("click", () => {
-    if (userTemp.value == "") {
-        result.textContent = `Please input a number`;
-    } else if (celToFahrenheit() == null || fahToCelcius() == null) {
-        result.textContent = `Please input an actual number`;
-    } else {
-        if (toFehrenheit.checked) {
-            result.textContent = `${celToFahrenheit()} ℉`;
-        } else if (toCelcius.checked) {
-            result.textContent = `${fahToCelcius()} ℃`;
-        } else {
-            result.textContent = `Please check one of the above options`;
-        }
-    }
-})
+rollDiceBtn.addEventListener("click", rollDice);
 
 
