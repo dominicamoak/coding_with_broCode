be791b6be2d22d256e677589a120954895bde237
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-08 16:35:14 +0100

    Javascript Setters and Getters

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 
 
 class Person {
-    constructor(name, job, role) {
+    static employees = 0;
+
+    constructor(name, age, job, role) {
         this.name = name;
+        this.age = age;
         this.job = job;
         this.role = role;
+        Person.employees++;
     }
 
     workplace() {
-        console.log(`${this.name} works at ${this.job}`);
+        console.log(`${this.name} is ${this.age} years old and works at ${this.job}`);
     }
 
     jobRole() {
         console.log(`${this.name} is a ${this.role}`);
     }
+
+    set age(newAge) {
+        if (typeof newAge === "string") {
+            console.error("Age has to be a number")
+        } else if (newAge >= 0) {
+            this._age = newAge;
+        } else {
+            console.error("Age cannot be less than 0");
+        }
+    }
+
+    get age() {
+        return this._age;
+    }
 }
 
-const person1 = new Person("Chris", "Google", "Software Engineer");
-const person2 = new Person("Prince", "Amazon", "Data Analyst");
+const person1 = new Person("Chris", 43, "Google", "Software Engineer");
+const person2 = new Person("Prince", 35, "Amazon", "Data Analyst");
 
 person1.workplace();
 person1.jobRole();
 
 person2.workplace();
 person2.jobRole();
+
+console.log(`Employees: ${Person.employees}`);
+console.log(person1.age);
 
 
