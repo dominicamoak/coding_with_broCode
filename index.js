802030c45f200e7c701386c122a130126c562c8f
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-11 00:07:13 +0100

    Javascript Clock Program works now

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 function timer() {
     const time = new Date();
-    let hours = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
+    let hours = time.getHours().toString().padStart(2, 0);
+    const minutes = time.getMinutes().toString().padStart(2, 0);
+    const seconds = time.getSeconds().toString().padStart(2, 0);
+    let timeframe = hours >= 12 ? "PM" : "AM";
 
-    document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds} ${timeframe}`;
 }
 
 timer();
+setInterval(timer, 1000);
 
