fa1c3142676e4578c96ab8e987635d3b1779e624
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-10 23:59:40 +0100

    Javascript Clock Program second commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 
+function timer() {
+    const time = new Date();
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
 
+    document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+timer();
 
