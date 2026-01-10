d6558039bb754fab51ed7d9a2a9aaba9ba60ff22
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-11 00:22:04 +0100

    Javascript Clock Program changed to 12 hour

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 function timer() {
     const time = new Date();
-    let hours = time.getHours().toString().padStart(2, 0);
+    let hours = time.getHours();
     const minutes = time.getMinutes().toString().padStart(2, 0);
     const seconds = time.getSeconds().toString().padStart(2, 0);
-    let timeframe = hours >= 12 ? "PM" : "AM";
+    const timeframe = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
 
     document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds} ${timeframe}`;
 }
