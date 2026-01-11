ce2d0ff813fcabccde6f5d46c0cca6ba2957bd13
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-11 13:41:41 +0100

    Javascript Stopwatch Program

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 
-function timer() {
-    const time = new Date();
-    let hours = time.getHours();
-    const minutes = time.getMinutes().toString().padStart(2, 0);
-    const seconds = time.getSeconds().toString().padStart(2, 0);
-    const timeframe = hours >= 12 ? "PM" : "AM";
-    hours = hours % 12 || 12;
-    hours = hours.toString().padStart(2, 0);
 
-    document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds} ${timeframe}`;
-}
-
-timer();
-setInterval(timer, 1000);
 
