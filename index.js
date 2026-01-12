c8823e1d64e228ec048bb59812be7ea4aa16948d
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-12 23:46:38 +0100

    Javascript Stopwatch Program Styled

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 
+const clock = document.getElementById("clock");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resetBtn = document.getElementById("resetBtn");
 
+
+function start() {
+
+}
+
+function stop() {
+
+}
+
+function reset() {
+    
+}
+
+function display() {
+    
+}
 
