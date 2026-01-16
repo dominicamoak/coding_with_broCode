fdae1269bbab9f1bb731840723087147e26e6f48
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-16 23:47:58 +0100

    Javascript Asynchronous and Synchronous

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 
+function aceSection(callback) {
+    setTimeout(() => {
+        console.log("Num 1");
+        callback()
+    }, 3000);
+}
 
+function syncSection() {
+    console.log("Num 2")
+    console.log("Num 3")
+    console.log("Num 4")
+    console.log("Num 5")
+}
+
+aceSection(syncSection);
 
