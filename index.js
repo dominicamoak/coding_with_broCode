acfbdc87fb0587502127796e6ed8e3f5082e0cdc
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-20 16:22:09 +0100

    Javascript Calculator Program working on logic

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 
+const display = document.getElementById("display");
+const buttons = document.getElementById("btns-container");
 
+//function calculatorDisplay() {
+//    switch(buttons) {
+//        case "+":
+//            display.textContent += "+";
+//            break;
+//    }
+//}
+
+
+
+buttons.addEventListener("click", (event) => {
+    const button = event.target;
+
+    if(button.tagName !== "BUTTON") return;
+
+    if(button === "=") {
+        let result = Number(display.value);
+        display.value = result;
+    } else if(button !== "=") {
+        display.value += button.textContent;
+    } 
+
+    
+
+    function calculate() {
+        Math.floor(Numbers(display.value));
+    }
+})
 
