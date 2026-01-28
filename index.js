cb405e5a8267aaa1f896bb592475f17bf28cf62b
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-28 19:04:13 +0100

    Javascript DOM Navigation

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 
+const fruitsElement = document.getElementById("fruits");
+const vegesElement = document.getElementById("vegetables");
+const drinksElement = document.getElementById("drinks");
 
+const fruitsFirstChild = fruitsElement.firstElementChild;
+fruitsFirstChild.style.backgroundColor = "yellow";
+
+const vegesFirstChild = vegesElement.firstElementChild;
+vegesFirstChild.style.backgroundColor= "pink";
+
+const drinksFirstChild = drinksElement.firstElementChild;
+drinksFirstChild.style.backgroundColor= "blue";
 
