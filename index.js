c9309abae29dd9004d1c4b05de9a7c701148b593
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-30 22:46:47 +0100

    Javascript key events more features

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ let xAxis = 0;
 let yAxis = 0;
 
 document.addEventListener("keydown", event => {
+    playBox.style.backgroundColor = "darkgoldenrod";
+    playBox.textContent = "😒";
+    playBox.style.boxShadow = "5px 5px 20px black";
+});
+
+document.addEventListener("keyup", event => {
+    playBox.style.backgroundColor = "hsl(240, 58%, 55%)";
+    playBox.textContent = "😊";
+    playBox.style.boxShadow = "5px 5px 20px white";
+});
+
+document.addEventListener("keydown", event => {
 
     console.log(event.key);
     if (event.key.startsWith("Arrow")) {
+
+        event.preventDefault();
+
         switch (event.key) {
             case "ArrowUp":
                 yAxis -= powerUp;
