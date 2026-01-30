3126d32c45cc0f55d17d425cd0daf55565287c4b
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-30 22:35:33 +0100

    Javascript key events

--- index.js
+++ index.js
@@ -1,49 +1,31 @@
 
-const fruitsElement = document.getElementById("fruits");
-const vegesElement = document.getElementById("vegetables");
-const drinksElement = document.getElementById("drinks");
+const playBox = document.getElementById("playBox");
 
-// firstChild using getElementByID
-const fruitsFirstChild = fruitsElement.firstElementChild;
-fruitsFirstChild.style.backgroundColor = "yellow";
+const powerUp = 20;
+let xAxis = 0;
+let yAxis = 0;
 
-const vegesFirstChild = vegesElement.firstElementChild;
-vegesFirstChild.style.backgroundColor= "pink";
+document.addEventListener("keydown", event => {
 
-const drinksFirstChild = drinksElement.firstElementChild;
-drinksFirstChild.style.backgroundColor= "blue";
+    console.log(event.key);
+    if (event.key.startsWith("Arrow")) {
+        switch (event.key) {
+            case "ArrowUp":
+                yAxis -= powerUp;
+                break;
+            case "ArrowDown":
+                yAxis += powerUp;
+                break;
+            case "ArrowLeft":
+                xAxis -= powerUp;
+                break;
+            case "ArrowRight":
+                xAxis += powerUp;
+                break;
+        }
 
-
-// lastChild using querySelectorAll
-const ulElements = document.querySelectorAll("ul");
-ulElements.forEach(ulElement => {
-    const lastChild = ulElement.lastElementChild;
-    lastChild.style.backgroundColor = "black";
-    lastChild.style.color = "white";
-})
-
-
-// nextSibling using getElementById
-const fruit = document.getElementById("mango");
-const nextSibling = fruit.nextElementSibling;
-nextSibling.style.backgroundColor = "green";
-
-// previousSibling using getElementById
-const vegetable = document.getElementById("onions");
-const previousSibling = vegetable.previousElementSibling;
-previousSibling.style.backgroundColor = "tomato";
-
-
-// parentElemnt using getElementById
-const element = document.getElementById("apple");
-const parent = element.parentElement;
-parent.style.border = "10px dotted black";
-
-
-// childrenElements using getElementById
-const drinks = document.getElementById("drinks");
-const drinksChildren = drinks.children;
-Array.from(drinksChildren).forEach(drink => {
-    drink.style.border = "5px dotted violet";
-})
+        playBox.style.top = `${yAxis}px`;
+        playBox.style.left = `${xAxis}px`;
+    }
+});
 
