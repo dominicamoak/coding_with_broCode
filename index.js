acb3c9ba5e74b8cb22bf91ffee2ca3ac4744f460
Dominic Amoak <amoakdominic76@gmail.com>
2026-01-31 00:34:15 +0100

    Javascript hiding DOM elements

--- index.js
+++ index.js
@@ -1,46 +1,14 @@
 
-const playBox = document.getElementById("playBox");
+const myImg = document.getElementById("myImg");
+const btn = document.getElementById("submitBtn");
 
-const powerUp = 20;
-let xAxis = 0;
-let yAxis = 0;
-
-document.addEventListener("keydown", event => {
-    playBox.style.backgroundColor = "darkgoldenrod";
-    playBox.textContent = "😒";
-    playBox.style.boxShadow = "5px 5px 20px black";
-});
-
-document.addEventListener("keyup", event => {
-    playBox.style.backgroundColor = "hsl(240, 58%, 55%)";
-    playBox.textContent = "😊";
-    playBox.style.boxShadow = "5px 5px 20px white";
-});
-
-document.addEventListener("keydown", event => {
-
-    console.log(event.key);
-    if (event.key.startsWith("Arrow")) {
-
-        event.preventDefault();
-
-        switch (event.key) {
-            case "ArrowUp":
-                yAxis -= powerUp;
-                break;
-            case "ArrowDown":
-                yAxis += powerUp;
-                break;
-            case "ArrowLeft":
-                xAxis -= powerUp;
-                break;
-            case "ArrowRight":
-                xAxis += powerUp;
-                break;
-        }
-
-        playBox.style.top = `${yAxis}px`;
-        playBox.style.left = `${xAxis}px`;
+btn.addEventListener("click", () => {
+    if (myImg.style.visibility === "hidden") {
+        myImg.style.visibility = "visible";
+        btn.textContent = "Hide";
+    } else {
+        myImg.style.visibility = "hidden";
+        btn.textContent = "Show";
     }
-});
+})
 
