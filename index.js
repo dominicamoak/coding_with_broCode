b7077caed841b137c75a6a93dfee80547b173335
Dominic Amoak <amoakdominic76@gmail.com>
2026-02-01 15:39:39 +0100

    Javascript Rock Paper Scissors game styling done

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 
-const myImg = document.getElementById("myImg");
-const btn = document.getElementById("submitBtn");
 
-btn.addEventListener("click", () => {
-    if (myImg.style.visibility === "hidden") {
-        myImg.style.visibility = "visible";
-        btn.textContent = "Hide";
-    } else {
-        myImg.style.visibility = "hidden";
-        btn.textContent = "Show";
-    }
-})
 
