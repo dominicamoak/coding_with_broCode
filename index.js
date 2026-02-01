79d6ab1849b484975aad80b7667de007f827a093
Dominic Amoak <amoakdominic76@gmail.com>
2026-02-01 17:32:09 +0100

    Javascript Rock Paper Scissors game logic written out

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 
+/// DOM ELEMENTS
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+
+const playerChoice = document.getElementById("playerChoice")
+const computerChoice = document.getElementById("computerChoice")
+
+const results = document.getElementById("results")
+
+const playerScore = document.getElementById("playerScore")
+const computerScore = document.getElementById("computerScore")
+
+/// RULES
+// rock beats scissors
+// scissors beats paper
+// paper beats rock
+
+/// LOGIC
+// 1. Player selects choice
+// 2. Compuetr choice is made exactly when the player choice is made
+// 3. Register both choices in the finalChoices div
+// 4. Check the rules for approapriate results using choices
+// 5. Display result in results h1
+// 6. Update the scoreboard
+
+/// CODE
+
 
 
