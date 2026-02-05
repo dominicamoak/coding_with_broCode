518d5e04d4ced562fb8040afbfc4c707fa6ed0ec
Dominic Amoak <amoakdominic76@gmail.com>
2026-02-05 18:54:34 +0100

    Javascript Image Slider - working on UI

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 
+const imageContainer = document.getElementById("imageContainer");
+const previousBtn = document.getElementById("previousBtn");
+const nextBtn = document.getElementById("nextBtn");
 
+const imagesURL = "/slider";
+let imagesArray = [];
+const imageIndex = 3;
+
+let image = document.createElement("img");
+image.src = `/slider/${imageIndex}.jpg`;
+
+imageContainer.appendChild(image);
 
 
 
