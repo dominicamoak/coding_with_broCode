5b808ba70f9581b5794b797b1f19efc422590ba5
Dominic Amoak <amoakdominic76@gmail.com>
2026-02-08 15:34:26 +0100

    Javascript - moved imageSlider to the appropriate files

---
+++ image_slider.js
@@ -0,0 +1,50 @@
+
+const previousBtn = document.getElementById("previousBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+const slides = document.querySelectorAll(".sliderImages img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initialize);
+nextBtn.addEventListener("click", nextSlide);
+previousBtn.addEventListener("click", previousSlide);
+
+function initialize() {
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add("currentSlide");
+        autoInterval();
+    }
+}
+
+function displaySlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach(slide => {
+        slide.classList.remove("currentSlide");
+    });
+    slides[slideIndex].classList.add("currentSlide");
+}
+
+function nextSlide() {
+    slideIndex++;
+    displaySlide(slideIndex);
+    autoInterval();
+}
+
+function previousSlide() {
+    clearInterval(intervalId);
+    slideIndex--;
+    displaySlide(slideIndex);
+}
+
+function autoInterval() {
+    clearInterval(intervalId);
+    intervalId = setInterval(nextSlide, 3000);
+}
+
+
+
