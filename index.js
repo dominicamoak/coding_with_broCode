81562661584979e218b492f8c60f576711b7ba03
Dominic Amoak <amoakdominic76@gmail.com>
2026-02-08 17:18:51 +0100

    Javascript - Promises

--- index.js
+++ index.js
@@ -1,50 +1,115 @@
 
-const previousBtn = document.getElementById("previousBtn");
-const nextBtn = document.getElementById("nextBtn");
+function learnJavaScript() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const learntJavaScript = true;
 
-const slides = document.querySelectorAll(".sliderImages img");
-let slideIndex = 0;
-let intervalId = null;
-
-document.addEventListener("DOMContentLoaded", initialize);
-nextBtn.addEventListener("click", nextSlide);
-previousBtn.addEventListener("click", previousSlide);
-
-function initialize() {
-    if (slides.length > 0) {
-        slides[slideIndex].classList.add("currentSlide");
-        autoInterval();
-    }
-}
-
-function displaySlide(index) {
-    if (index >= slides.length) {
-        slideIndex = 0;
-    } else if (index < 0) {
-        slideIndex = slides.length - 1;
-    }
-    slides.forEach(slide => {
-        slide.classList.remove("currentSlide");
+            if (learntJavaScript) {
+                resolve("I feel confident with JavaScript! 💪🏽");
+            } else {
+                reject("I don't feel confident with JS 😭");
+            }
+        }, 3000);
     });
-    slides[slideIndex].classList.add("currentSlide");
 }
 
-function nextSlide() {
-    slideIndex++;
-    displaySlide(slideIndex);
-    autoInterval();
+function learnTypeScript() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const learntTypeScript = true;
+
+            if (learntTypeScript) {
+                resolve("I feel confident with TypeScript! 💪🏽");
+            } else {
+                reject("I don't feel confident with TypeScript 😭");
+            }
+        }, 1500);
+    });
 }
 
-function previousSlide() {
-    clearInterval(intervalId);
-    slideIndex--;
-    displaySlide(slideIndex);
+function learnPython() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const learntPython = true;
+
+            if (learntPython) {
+                resolve("I feel confident with Python! 💪🏽");
+            } else {
+                reject("I don't feel confident with Python 😭")
+            }
+        }, 2000);
+    });
 }
 
-function autoInterval() {
-    clearInterval(intervalId);
-    intervalId = setInterval(nextSlide, 3000);
+function learnSQL() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const learntSQL = true;
+
+            if (learntSQL) {
+                resolve("I feel confident with SQL! 💪🏽");
+            } else {
+                reject("I don't feel confident with SQL 😭")
+            }
+        }, 1500);
+    });
 }
+
+function learnNodeJS() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const learntNodeJS = true;
+
+            if (learntNodeJS) {
+                resolve("I feel confident with NodeJS! 💪🏽");
+            } else {
+                reject("I don't feel confident with NodeJS 😭")
+            }
+        }, 2500);
+    });
+}
+
+function learnC() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const learntC = true;
+
+            if (learntC) {
+                resolve("I feel confident with C! 💪🏽");
+            } else {
+                reject("I don't feel confident with C 😭")
+            }
+        }, 3000);
+    });
+}
+
+learnJavaScript()
+.then(value => {
+    console.log(value);
+    return learnTypeScript();
+})
+.then(value => {
+    console.log(value); 
+    return learnPython();
+})
+.then(value => {
+    console.log(value); 
+    console.log("Apply for Software Engineer roles 😌")
+    return learnNodeJS();
+})
+.then(value => {
+    console.log(value); 
+    return learnSQL()
+})
+.then(value => {
+    console.log(value); 
+    return learnC()
+})
+.then(value => {
+    console.log(value); 
+    console.log("Feeling good as a Programmer! 🫡")
+})
+.catch(error => console.error(error));
 
 
 
